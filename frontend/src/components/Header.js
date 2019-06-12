6f090688019aad19b5b31835549bf8a0c6e50148
Roberto Rosa Jr <beto.junior@gmail.com>
2019-06-12 19:25:26 -0300

    Aula 3 fim
    acompanhamento aula 3 finalizado

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 
@@ -6,12 +7,16 @@ import logo from '../assets/logo.svg';
 import camera from '../assets/camera.svg';
 
 export default function Header() {
-  return (
-     <header id='main-header'>
-        <div className='header-content'>
-            <img src={logo} alt="InstaRocket"/>
-            <img src={camera} alt="Enviar Publicaçao"/>
-        </div>
-     </header> 
-  );
+   return (
+      <header id='main-header'>
+         <div className='header-content'>
+            <Link to="/">
+               <img src={logo} alt="InstaRocket" />
+            </Link>
+            <Link to="/new" >
+               <img src={camera} alt="Enviar Publicaçao" />
+            </Link>
+         </div>
+      </header>
+   );
 }
